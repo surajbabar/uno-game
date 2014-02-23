d4a47ddb501d1cc049fef49ca1b64ae0a0baa555
surajbabar <surajbab@thoughtworks.com>
2014-02-23 13:03:46 +0530

    changed names- server to Master and client to Player

--- src/com/step/uno/client/UnoPlayer.java
+++ src/com/step/uno/client/UnoPlayer.java
@@ -4,7 +4,7 @@ import com.step.communication.factory.UnoFactory;
 import com.step.uno.client.controller.GameClientController;
 import com.step.uno.client.screen.JoinGameScreen;
 
-public class UnoClient {
+public class UnoPlayer {
     public static void main(String[] args) {
 
         UnoFactory factory = new UnoFactory();
