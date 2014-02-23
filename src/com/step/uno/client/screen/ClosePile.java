2f65451f712d3db738e707ae0ea2e5f6f520ea0a
surajbabar <surajbab@thoughtworks.com>
2014-02-23 14:53:44 +0530

    now independent of model ,takes action listener as a parameter

--- src/com/step/uno/client/screen/ClosePile.java
+++ src/com/step/uno/client/screen/ClosePile.java
@@ -1,46 +1,23 @@
 package com.step.uno.client.screen;
 
-import com.step.uno.client.model.GameClient;
-import com.step.uno.messages.Snapshot;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ClosePile extends JPanel {
-
     private JButton draw;
-    private GameClient gameClient;
-    private Snapshot snapshot;
 
-    public ClosePile(GameClient gameClient, Snapshot snapshot) {
-        this.gameClient = gameClient;
-
-        this.snapshot = snapshot;
-
+    public ClosePile(boolean isMyTurn,ActionListener ClosePileController) {
         draw = new JButton("Draw Cards");
         draw.setPreferredSize(new Dimension(100, 150));
-        draw.setEnabled(snapshot.currentPlayerIndex == snapshot.myPlayerIndex);
-
-        draw.addActionListener(new OnClick());
-        draw.setFont(new Font("vardana", Font.BOLD, 18));
+        draw.setEnabled(isMyTurn);
+        draw.addActionListener(ClosePileController);
+        draw.setFont(new Font("verdana", Font.BOLD, 18));
         add(draw);
         setVisible(true);
-
     }
 
-    public void update(Snapshot snapshot) {
-        this.snapshot = snapshot;
-        draw.setEnabled(snapshot.currentPlayerIndex == snapshot.myPlayerIndex);
+    public void update(boolean isMyTurn) {
+        draw.setEnabled(isMyTurn);
     }
-
-    private class OnClick implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            gameClient.draw();
-        }
-
-    }
-
 }
